01733ff36da0d41eafdc0e97215942fa5b54172e
unknown <jacqueline_rwa@msn.com>
2018-01-20 18:58:44 +0100

    done

--- canvas.js
+++ canvas.js
@@ -1,22 +1,3 @@
-// var c = document.getElementById("mon_canvas");
-// var ctx = c.getContext("2d");
-// // ici, le reste du script
-//
-// context.fillStyle = "gold";
-// context.fillRect(0, 0, 50, 80);
-// ctx.beginPath();
-// ctx.moveTo(50,50);
-// ctx.lineTo(200,200);
-// ctx.moveTo(200,50);
-// ctx.lineTo(50,200);
-// ctx.closePath();
-//
-// ctx.fillStyle = "red";
-// ctx.strokeStyle = "#ecf0f1";
-//
-// ctx.lineJoin = "bevel";
-// ctx.lineCap = "round";
-
 var canvas = document.getElementById("mon_canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -54,24 +35,114 @@ var c = canvas.getContext('2d');
 //   c.stroke();
 // }
 
-var x = 200;
-var dx = 5; //speed
-var radius = 30;
-function animate(){ //nom de la fonction: de notre choix
-  requestAnimationFrame(animate); //cree une loop qui appelle l'animation
-  console.log("lolilol");
-  c.clearRect (0, 0, innerWidth, innerHeight);
+var mouse = {
+  x: undefined,
+  y: undefined
+}
 
+var maxRadius = 40;
+var minRadius = 2;
+
+var colorArray = [
+  "#320D6D",
+  "#FFBFB7",
+  "#FFD447",
+  "#700353",
+  "#4C1C00",
+];
+
+window.addEventListener('mousemove', // action de la fonction des mouvement de souris
+  function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+  //  console.log(mouse); donne les infos (objets) de la souris
+})
+
+window.addEventListener('resize', function (){
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  init(); // appel de la fonction creee qui relance
+})
+
+function Circle(x, y, dx, dy, radius){ //creation de l'objet cercle
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+  // this.minRadius = minRadius;
+  this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
+
+  this.draw = function(){ // creation de la fonction dessiner cercke
     c.beginPath();
-    c.arc(x, 200, radius, 0, Math.PI * 2, false);
-    c.strokeStyle = "teal";
-    c.stroke();
-
-  if(x + radius >innerWidth || x - radius < 0){
-    dx = -dx;
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    // c.strokeStyle = "teal";
+    // c.stroke();
+    c.fillStyle = this.color;
+    c.fill();
   }
-  x += dx;
+
+  this.update = function(){ // donne de nouvelles valeurs au cercle
+    if(this.x + this.radius >innerWidth || this.x - this.radius < 0){ //empeche le cercle de sortir de l'écran (entre 0 debut et innerwidth fin)
+      this.dx = -this.dx;
+    }
+
+    if(this.y + this.radius >innerHeight || this.y - this.radius < 0){
+      this.dy = -this.dy;
+    }
+
+    this.x += this.dx;
+    this.y += this.dy;
+
+    // effet lors de survol de souris
+    if (mouse.x - this.x < 50 && mouse.x - this.x > -50 // la coord x de la souris // la coord de la bulle/cercle survolé (de part et d'autres d'ou -)
+        && mouse.y - this.y < 50 && mouse.y - this.y > -50
+    ){
+      if(this.radius < maxRadius){ //grossir jusqu'à une certaine taille
+        this.radius +=1; // +gros
+      }
+    }
+    else if(this.radius > minRadius){ // les reduires jusqu'à un certain radius, taille l'initial
+      this.radius -=1;
+    }
+
+    this.draw(); // appelle creation de cercle
+  }
+}
+
+var circleArray = [];
+
+function init(){ // lance la creation
+
+  circleArray = []; //nettoie l'ecran
+  var nbreCircle = 2000;
+
+  for (var i=0; i<nbreCircle; i++){ // donne valeur ramdom au coord des cercles
+    // creation du cercle random
+    var radius = Math.random() * 3 + 1;
+    var x = Math.random() * (innerWidth - radius * 2) + radius; //bord des cercles ricochent sur bord droit + gauch de l'écran
+    var y = Math.random() * (innerHeight - radius * 2) + radius;
+    var dx = (Math.random() - 0.5) * 5; //speed
+    var dy = (Math.random() - 0.5) * 5;
+
+    circleArray.push(new Circle(x, y, dx, dy, radius)) // ajout du nouveau cercle dans le tableau des cercles
+  }
+  // console.log(circleArray);
 }
 
 
-// animate();
+
+function animate(){ //nom de la fonction: de notre choix
+  requestAnimationFrame(animate); //cree une loop qui appelle l'animation
+  // console.log("lolilol");
+  c.clearRect (0, 0, innerWidth, innerHeight);
+
+  for (var i=0; i<circleArray.length; i++){ // creation de nouveaux cercles
+    circleArray[i].update();
+  }
+
+}
+
+init();
+animate();
